2cb956d4f253e7fa92ef4c5b33cac7f89dda1ab7
kitaka Grace <34123938+prophetgrace@users.noreply.github.com>
2019-06-03 02:24:35 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+let a = [7, 4, 24, 90, 21];
+//Sort arranges in alphabetical order
+console.log(a.sort());
+
+a.push(76);
+console.log(a);
+
+a.push("goo"); //Adds element at the end
+console.log(a);
+
+a.pop(); //Removes last Element
